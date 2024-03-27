10edcd38fe7f699ca04436ab5692198aa458ee33
brunor2640 <bruno.r.monteiro7@aluno.senai.br>
2024-03-26 21:47:01 -0300

    26/03/24 colocando estilo na tela de login

---
+++ site-teste/scripts/form-login.js
@@ -0,0 +1,26 @@
+function validarLogin() {
+    let login = document.querySelector("#login").value;
+    console.log(login);
+    let senha = document.querySelector("#senha").value;
+    console.log(senha);
+    let msg = document.querySelector("span");
+    msg.innerText = "Login ou Senha incorreto!";
+     //   document.querySelector("button").addEventListener("click", validarLogin);
+    // se login = senai e senha = 010203
+    //entao acesso permitido
+    //senao login ou senha incorreto &&
+    if (login == "senai" && senha == "010203"){
+        msg.innerHTML = "Acesso Permitido <a href='index.html'> Clique aqui</a>";
+        msg.style = "background-color: blue"
+         window.location.href = "index.html"
+   }
+ else {
+
+        msg.innerText = "Login ou Senha incorreto!";
+        msg.style = "background-color: red";
+ }
+
+    return false;
+
+  }
+  
